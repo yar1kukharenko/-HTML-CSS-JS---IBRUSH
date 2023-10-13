25256ff25cf18450a9fc575910f52ee3835f60fb
Iaroslav Kukharenko <113698528+yar1kukharenko@users.noreply.github.com>
2023-10-13 14:00:48 +0300

    done

--- src/js/index.js
+++ src/js/index.js
@@ -1,5 +1,12 @@
 import mobileNav from './modules/mobile-nav.js';
 import Choices from 'choices.js';
+// import Swiper bundle with all modules installed
+import Swiper from 'swiper/bundle';
+
+// import styles bundle
+import 'swiper/css/bundle';
+
+// init Swiper:
 
 mobileNav();
 
@@ -20,4 +27,12 @@ const element = document.querySelector('.js-choice');
 const choices = new Choices(element, {
 	searchEnabled: false,
 	shouldSort: false,
+});
+
+const swiper = new Swiper('.swiper', {
+	slidesPerView: 1,
+	pagination: {
+		el: '.swiper-pagination',
+		clickable: true,
+	},
 });
