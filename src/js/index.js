a4f5875450fbf9fb4d87347fa7e0a0658d210274
Iaroslav Kukharenko <113698528+yar1kukharenko@users.noreply.github.com>
2023-10-13 12:15:07 +0300

    added feedback

--- src/js/index.js
+++ src/js/index.js
@@ -1,2 +1,23 @@
 import mobileNav from './modules/mobile-nav.js';
+import Choices from 'choices.js';
+
 mobileNav();
+
+// Получаем все радио-кнопки с именем "rating"
+const ratingItems = document.querySelectorAll('input[name="stars"]');
+
+// Получаем элемент для отображения значения
+const selectedValueElement = document.getElementById('selectedValue');
+
+// Добавляем обработчик события для каждой радио-кнопки
+ratingItems.forEach((item) => {
+	item.addEventListener('change', function () {
+		selectedValueElement.innerText = this.value;
+	});
+});
+
+const element = document.querySelector('.js-choice');
+const choices = new Choices(element, {
+	searchEnabled: false,
+	shouldSort: false,
+});
